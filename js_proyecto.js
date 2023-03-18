7a41621cf9f4d17cb7a9aa687f09f5a7b034a732
MatiasAltamirano <matiasalt5@gmail.com>
2023-03-18 15:45:15 -0300

    Primera version del CV proyecto integrador

---
+++ js_proyecto.js
@@ -0,0 +1,22 @@
+function edad(){
+    var divInfo = document.getElementById("reemplazar");
+    divInfo.innerText = "Actualmente tengo 28 años de edad"
+}
+function email(){
+    var divInfo = document.getElementById("reemplazar");
+    divInfo.innerText = "Mi direccion de correo electronico es: deanmitchel5@gmail.com"
+}
+function locacion(){
+    var divInfo = document.getElementById("reemplazar");
+    divInfo.innerText = "Avenida San Martin 548 - Barrio los Alerces"
+}
+function telefono(){
+    var divInfo = document.getElementById("reemplazar");
+    divInfo.innerText = "Puede contactarse al siguiente número: 011-8739187"
+}
+function licencia(){
+    var divInfo = document.getElementById("reemplazar");
+    divInfo.innerText = "Tengo habilitación 'B2' para conducir distintos tipos de vehiculos"
+}
+
+/**archivo js para cv proyecto*/
